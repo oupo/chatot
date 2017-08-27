93ba6300eebcfe0e78fd199649709b854386df1b
oupo <oupo.nejiki@gmail.com>
2017-08-28 01:21:04 +0900

    update shim.js

--- shim.js
+++ shim.js
@@ -1,6 +1,8 @@
 navigator.getUserMedia = ( navigator.getUserMedia ||
                        navigator.webkitGetUserMedia ||
                        navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
+                       navigator.msGetUserMedia) || 
+                       function() { throw "navigator.getUserMedia doesn't support navigator.getUserMedia" };
+
 
 window.AudioContext = window.AudioContext||window.webkitAudioContext;
